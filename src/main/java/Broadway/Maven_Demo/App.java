a182f4d8c0be8a9f573ec7bfb76f9bc24eb6254e
Sudershan Khadka <106295087+sudershankhhadka@users.noreply.github.com>
2024-02-20 18:50:54 +0545

    maven initials

---
+++ src/main/java/Broadway/Maven_Demo/App.java
@@ -0,0 +1,13 @@
+package Broadway.Maven_Demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
